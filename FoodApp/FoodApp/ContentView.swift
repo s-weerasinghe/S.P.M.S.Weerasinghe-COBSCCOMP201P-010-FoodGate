49e5eb62a1578620be592a27ff79c842a7d825d0
s-weerasinghe <weerasinghesandaruwani@gmail.com>
2023-01-13 12:39:18 +0530

    firebase + start screen

---
+++ FoodApp/FoodApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodApp
+//
+//  Created by Sweerasinghe on 1/13/23.
+//  Copyright © 2023 Sweerasinghe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
